4c233b1b7eab8512b1df4ec69407a72ecb4c5478
HumphryTlou <163722862+HumphryTlou@users.noreply.github.com>
2024-06-08 17:36:03 +0200

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -1,16 +1,17 @@
-BASE_URL = "/accounts" 
+BASE_URL = "/accounts"
 
 """
 Account Service
 
 This microservice handles the lifecycle of Accounts
 """
-# pylint: disable=unused-import
+
 from flask import jsonify, request, make_response, abort, url_for   # noqa; F401
 from service.models import Account
 from service.common import status  # HTTP Status Codes
 from . import app  # Import Flask application
 
+# pylint: disable=unused-import
 
 ############################################################
 # Health Endpoint
@@ -44,7 +45,7 @@ def index():
 def create_accounts():
     """
     Creates an Account
-    This endpoint will create an Account based the data in the body that is posted
+    This endpoint will create an Account based on the data in the body that is posted
     """
     app.logger.info("Request to create an Account")
     check_content_type("application/json")
@@ -54,7 +55,7 @@ def create_accounts():
     message = account.serialize()
     # Uncomment once get_accounts has been implemented
     location_url = url_for("get_accounts", account_id=account.id, _external=True)
-    #location_url = "/"  # Remove once get_accounts has been implemented
+    # location_url = "/"  # Remove once get_accounts has been implemented
     return make_response(
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
@@ -79,12 +80,11 @@ def list_accounts():
 ######################################################################
 # READ AN ACCOUNT
 ######################################################################
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
 def read_accounts(account_id):
     """
     Reads an Account
-    This endpoint will read an Account based the account_id that is requested
+    This endpoint will read an Account based on the account_id that is requested
     """
     app.logger.info("Request to read an Account with id: %s", account_id)
 
@@ -139,8 +139,6 @@ def delete_accounts(account_id):
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
 ######################################################################
-
-
 def check_content_type(media_type):
     """Checks that the media type is correct"""
     content_type = request.headers.get("Content-Type")
