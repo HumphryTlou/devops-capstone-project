793e2733c2f25fba4ed28d00923f0366dfebe8c0
HumphryTlou <humphrytlou@gmail.com>
2024-06-08 14:16:17 -0400

    completed ci build

--- service/routes.py
+++ service/routes.py
@@ -114,6 +114,7 @@ def update_accounts(account_id):
 
     return account.serialize(), status.HTTP_200_OK
 
+
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
@@ -130,6 +131,7 @@ def delete_accounts(account_id):
         account.delete()
 
     return "", status.HTTP_204_NO_CONTENT
+
 
 ######################################################################
 #  U T I L I T Y   F U N C T I O N S
